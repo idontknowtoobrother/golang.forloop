ea8374223994e7bf12897b8182d2b395df168dac
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-04-23 18:24:35 +0700

    while loop with pre and post plus index

--- index.go
+++ index.go
@@ -4,10 +4,28 @@ import "fmt"
 
 func main() {
 	forLoopIndex()
+	whileLoopPostPlusIndex()
+	whileLoopPrePlusIndex()
 }
 
 func forLoopIndex() {
 	for i := 0; i < 10; i++ {
+		fmt.Printf("\nindex at %d", i)
+	}
+}
+
+func whileLoopPostPlusIndex() {
+	i := 0
+	for i < 10 {
+		fmt.Printf("\nindex at %d", i)
+		i++ // post plus
+	}
+}
+
+func whileLoopPrePlusIndex() {
+	i := 0
+	for i < 10 {
+		i++ // pre plus
 		fmt.Printf("\nindex at %d", i)
 	}
 }
